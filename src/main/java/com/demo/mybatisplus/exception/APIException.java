dea43ce6f98cc16f21e5daaf45f08c3dd70aa9d7
zhangmilin <908980328@qq.com>
2020-07-02 14:15:07 +0800

    删除lombok的依赖

--- src/main/java/com/demo/mybatisplus/exception/APIException.java
+++ src/main/java/com/demo/mybatisplus/exception/APIException.java
@@ -1,8 +1,5 @@
 package com.demo.mybatisplus.exception;
 
-import lombok.Getter;
-
-@Getter //只要getter方法，无需setter
 public class APIException extends RuntimeException {
     private int code;
     private String msg;
@@ -18,6 +15,22 @@ public class APIException extends RuntimeException {
     public APIException(int code, String msg) {
         super(msg);
         this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
         this.msg = msg;
     }
 }
